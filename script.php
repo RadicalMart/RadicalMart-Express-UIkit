35ff33e2651cff895d4b204a918d46f2198d91f7
Igor Berdichevskiy <iberdichevskiy@septdir.com>
2023-04-14 16:36:59 +0300

    Reset to dev

--- script.php
+++ script.php
@@ -2,7 +2,7 @@
 /*
  * @package     RadicalMart Express Uikit Package
  * @subpackage  pkg_radicalmart_express_uikit
- * @version     1.0.0
+ * @version     __DEPLOY_VERSION__
  * @author      Delo Design - delo-design.ru
  * @copyright   Copyright (c) 2023 Delo Design. All rights reserved.
  * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
